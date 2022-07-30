20baa2ea86eead64263ea490ffc569a4ad64a0ed
Shubham_Bohra <shubhambohra0009@gmail.com>
2022-07-30 23:24:39 +0530

    tuto 16 completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,70 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
+import AboutNew from './components/AboutNew';
+import Alert from './components/Alert';
+import React, {useState} from 'react'
+// import {Routes} from 'react-router-dom';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
 
 function App() {
+
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);  
+
+ const showAlert = (message,type)=>{
+  setAlert({
+    msg: message,
+    type: type
+  })
+  setTimeout(() => {
+    setAlert(null);
+  }, 1500);
+ };
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode("dark");
+      document.body.style.backgroundColor = '#042743';
+      // document.body.style.backgroundColor = 'grey';
+      showAlert("Dark mode has been enabled", "success");
+      // document.title = 'TextUtils - Dark Mode'
+    }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+      // document.title = 'TextUtils - Light Mode'
+    }
+  }
+
   return (
     <>
+    <Router>
 {/* <Navbar/> */}
-{/* <Navbar title="TextUtils" /> */}
-<Navbar title="TextUtils" aboutText="About TextUtils"/>
+{/* <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode}/> */}
+<Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+<Alert alert={alert}/> 
   
 <div className="container my-3">
-<TextForm heading="Enter the text to analyze"/>
+          <Routes>
+            {/* /users ---> Component 1 
+            /users/home ---> Component 2 */}
+              <Route exact path='/about' element={<AboutNew mode={mode}/>}/>
+              <Route exact path="/"  element={<TextForm heading="Enter the text to analyze" mode={mode} showAlert={showAlert}/>} />
+        </Routes>
+{/* <AboutNew mode={mode}/> */}
 </div>
+    </Router>
+
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,20 +3,44 @@ import React, {useState} from 'react'
 //here count is a variable with having default value which is 0 given in the bracket and setCount will be the function
 // that will be used for updating the value of count. Hooks let us use state and other React features without writing a class. 
 
-
-
 export default function TextForm(props) {
 
   const handleUpClick = ()=>{
     // console.log("Uppercase was clicked :" + text);
     let newText = text.toUpperCase();
     setText(newText);
+    props.showAlert("Converted to UpperCase!", "success");
   }
   const handleLoClick = ()=>{
     // console.log("Lowercase was clicked :" + text);
     let newText = text.toLowerCase();
     setText(newText);
+    props.showAlert("Converted to LowerCase!", "success");
   }
+  const handleClearClick = ()=>{
+    let newText = ("");
+    setText(newText);
+    props.showAlert("Text Cleared!", "success");
+  }
+  const handleCapitalizeClick = ()=>{
+    let newText = text.replace(/\b\w/g, function(l){ return l.toUpperCase() });
+    setText(newText);
+    props.showAlert(" Converted to Capitalize!", "success");
+  }
+  
+  const handleCopy = ()=>{
+    let text= document.getElementById('myBox');
+    // text.select();
+    // text.setSelectionRange(0,9999);
+    navigator.clipboard.writeText(text.value);
+    props.showAlert("Copied to Clipboard!", "success");
+  }
+  const handleExtraSpaces = ()=>{
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+    props.showAlert("Extra Spaces Removed!", "success");
+  }
+
   const handleOnChange = (event)=>{
     // console.log("On change");
     setText(event.target.value);
@@ -28,21 +52,25 @@ export default function TextForm(props) {
   // setText("new text"); // Correct way to change the state
   return (
     <>
-    <div className='container'>
+    <div className='container' style={{color : props.mode==="dark" ? 'white' : 'black'}}>
     <h1>{props.heading}</h1>
 <div className="mb-3">
-  <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+  <textarea className="form-control" value={text} onChange={handleOnChange} style ={{backgroundColor : props.mode==="dark" ? '#042743' : 'white' , color : props.mode==="dark" ? 'white' : 'black' }}  id="myBox" rows="8"></textarea>
 </div>
 <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
 <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
+<button className="btn btn-primary mx-1" onClick={handleCapitalizeClick}>Capitalize</button>
+<button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
+<button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+<button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
     </div>
 
-    <div className="container my-3">
+    <div className="container my-3" style={{color : props.mode==="dark" ? 'white' : 'black'}}>
       <h2>Your text Summary</h2>
-      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{text.length>0 ? text.split(" ").length : "0"} words and {text.length} characters</p>
       <p>{0.008 * text.split(" ").length} Minutes read</p>
       <h2>Preview</h2>
-      <p>{text}</p>
+      <p>{text.length>0 ?text : "Enter something in the textbox above to preview it here "}</p>
     </div>
     </>
   )
