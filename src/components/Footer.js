3e0fbfda3f0e9adfedb26c479d8895727b915104
Shubham_Bohra <shubhambohra0009@gmail.com>
2023-10-14 19:34:24 +0530

    fixed babel dependency

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer({darkmode}){
   <footer className={`text-center text-lg-start text-${darkmode? 'white' : 'dark'}`} style={{backgroundColor: darkmode? '#1c2331': '#f8f9fa',textAlign: 'center'}}>
     <div className="text-center p-2" style={{backgroundColor: 'rgba(0, 0, 0, 0.1)', height: '48px', }}>
       © {year} Copyright:  
-      <a className={`text-${darkmode? 'white' : 'dark'} mx-2`} href="https://www.linkedin.com/in/shubham-bohra-17ba94201/" target="_blank" style={{textDecoration : 'none', borderBottom : `1px solid ${!darkmode ? "black " : 'white'}`}} >Shubham Bohra</a>
+      <a className={`text-${darkmode? 'white' : 'dark'} mx-2`} href="https://www.linkedin.com/in/shubham-bohra-17ba94201/" target="_blank" rel="noopener noreferrer" style={{textDecoration : 'none', borderBottom : `1px solid ${!darkmode ? "black " : 'white'}`}} >Shubham Bohra</a>
     </div>
   </footer>
 </div>
